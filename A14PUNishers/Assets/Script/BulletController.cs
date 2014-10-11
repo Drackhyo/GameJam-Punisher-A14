33063ef06ca6d44a8a72ae3584cac141954a8d6a
Drackhyo <drackhyo@gmail.com>
2014-10-10 22:12:56 -0400

    Emo bullet

---
+++ A14PUNishers/Assets/Script/BulletController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletController : MonoBehaviour {
+	public float timeToExpire=7;
+	private float timeElapsed=0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeElapsed+=Time.deltaTime;
+		if(timeElapsed>timeToExpire)
+		{
+			Destroy (gameObject);
+		}
+	
+	}
+}
