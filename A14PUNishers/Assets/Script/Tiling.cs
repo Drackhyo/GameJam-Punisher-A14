7155cfa78badc4f68fbbc4410df2f727e6ae8398
gabrielhebert <ghebert10@gmail.com>
2014-10-10 22:30:47 -0400

    stuff

---
+++ A14PUNishers/Assets/Script/Tiling.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(SpriteRenderer))]
+
+public class Tiling : MonoBehaviour {
+
+	public int offsetX = 2;
+
+	public bool hasARightBuddy = false;
+	public bool hasALeftBuddy = false;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
