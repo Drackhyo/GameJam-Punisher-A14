751cf277fc2f61cbb018415edf78aceb1a268fb7
Drackhyo <drackhyo@gmail.com>
2014-10-11 02:04:53 -0400

    Stupidité artificiall

--- A14PUNishers/Assets/Script/AIController.cs
+++ A14PUNishers/Assets/Script/AIController.cs
@@ -6,7 +6,9 @@ public class AIController : MonoBehaviour {
 	private Transform player;
 	private List<Transform> targets;
 	private float timeSinceLastShot=0;
+	private bool facingRight=true;
 
+	public float moveSpeed=4;
 	public GameObject BulletPrefab;
 	public float shotDelay=2;
 	// Use this for initialization
@@ -20,25 +22,52 @@ public class AIController : MonoBehaviour {
 	void Update () 
 	{
 		timeSinceLastShot+=Time.deltaTime;
-		if(timeSinceLastShot>=shotDelay)
+		Vector3 newPosition = transform.position;
+		if(facingRight)
 		{
+			newPosition.x += moveSpeed * Time.deltaTime; 
 			Vector3 spawnpos=transform.position;
-			timeSinceLastShot=0;
-			if(player.position.x<transform.position.x)
+			spawnpos.x+=(collider2D.bounds.size.x/1.5f);
+			if(timeSinceLastShot>=shotDelay)
 			{
-
-				spawnpos.x-=1;
+				if((player.position.x>=transform.position.x))
+				{
+					timeSinceLastShot=0;
+					GameObject bulletShot=GameObject.Instantiate(BulletPrefab,spawnpos,transform.rotation)as GameObject;
+					Vector2 tempVector= new Vector2((player.position.x - transform.position.x),player.position.y - transform.position.y);
+					tempVector.Normalize();
+					tempVector=tempVector*8;
+					bulletShot.rigidbody2D.velocity= tempVector; 
+				}
 			}
-			else
-			{
-				spawnpos.x+=0.75f;
-			}
-			GameObject bulletShot=GameObject.Instantiate(BulletPrefab,spawnpos,transform.rotation)as GameObject;
-			Vector2 tempVector= new Vector2((player.position.x - transform.position.x),player.position.y - transform.position.y);
-			tempVector.Normalize();
-			tempVector=tempVector*8;
-			bulletShot.rigidbody2D.velocity= tempVector; 
-
 		}
+		else
+		{
+			newPosition.x -= moveSpeed * Time.deltaTime; 
+			Vector3 spawnpos=transform.position;
+			spawnpos.x-=(collider2D.bounds.size.x/1.5f);
+			if(timeSinceLastShot>=shotDelay)
+			{
+				if((player.position.x<=transform.position.x))
+				{
+					timeSinceLastShot=0;
+					GameObject bulletShot=GameObject.Instantiate(BulletPrefab,spawnpos,transform.rotation)as GameObject;
+					Vector2 tempVector= new Vector2((player.position.x - transform.position.x),player.position.y - transform.position.y);
+					tempVector.Normalize();
+					tempVector=tempVector*8;
+					bulletShot.rigidbody2D.velocity= tempVector; 
+				}
+			}
+		}
+		transform.position = newPosition;
+
+	}
+	public void Flip ()
+	{
+		facingRight = !facingRight;
+
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
 	}
 }
---
+++ A14PUNishers/Assets/Script/PlatformChecker.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformChecker : MonoBehaviour {
+	private bool isOnPlatform=true;
+	private float counter=0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		counter+=Time.deltaTime;
+		if (counter>=0.1f)
+		{
+			counter=0;
+			if(!isOnPlatform)
+				{
+					transform.parent.gameObject.GetComponent<AIController>().Flip();
+				}
+				isOnPlatform=false;
+	
+		}
+	}
+	void OnTriggerStay2D()
+	{
+		isOnPlatform=true;
+	}
+}
