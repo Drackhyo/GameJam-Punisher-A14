a2078a8453f2e31697aa411a88c89c68c1e98c40
maximetremblay1990 <maxime.tremblay1990@outlook.com>
2014-10-11 04:13:55 -0400

    peniiiii

---
+++ A14PUNishers/Assets/Script/EnemyState.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyStats : MonoBehaviour {
+
+	int health;
+
+	void Start()
+	{
+		if (rigidbody2D.gameObject.tag == "Human")
+			health = 3;
+		else
+			health = 6;
+	}
+
+	void Update()
+	{
+		if (health <= 0)
+			Dies();
+	}
+
+	void Dies()
+	{
+		if (rigidbody2D.gameObject.tag == "Human")
+		{
+			BecomeCorpse();
+		}
+		else
+		{
+			//demon death
+		}
+	}
+
+	void BecomeCorpse()
+	{
+
+	}
+}
---
+++ A14PUNishers/Assets/Script/PlayerState.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStats : MonoBehaviour {
+
+	int health = 3;
+	int lives = 3;
+
+	bool hasHead = true;
+	bool hasArms = true;
+	bool hasLegs = true;
+
+	void Update()
+	{
+		if (health <= 0)
+			Dies();
+	}
+
+	void TakeDamage(int amount)
+	{
+		health -= amount;
+	}
+
+	void Dies()
+	{
+		if (lives > 0)
+		{
+			lives--;
+			Respawn();
+		}
+		else
+		{
+		    //gameover
+		}
+	}
+
+	void Respawn()
+	{
+		//Change position
+		//Full health
+	}
+}
