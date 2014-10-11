98aae26370956bbe325b2c5f6d8a2fefff70146b
Drackhyo <drackhyo@gmail.com>
2014-10-11 13:50:23 -0400

    DemonAI

--- A14PUNishers/Assets/Script/AIController.cs
+++ A14PUNishers/Assets/Script/AIController.cs
@@ -9,12 +9,16 @@ public class AIController : MonoBehaviour {
 	private bool facingRight=true;
 	private float stunnedTime=0;
 
+	float isAttackingTimer=1;
+	Vector3 spawnpos;
+
 	public float stunDelay=1;
 	public float moveSpeed=4;
 	public GameObject BulletPrefab;
 	public float shotDelay=2;
 
 	EnemyState enemyState;
+	Animator anim;
 
 	// Use this for initialization
 	void Start () {
@@ -22,53 +26,69 @@ public class AIController : MonoBehaviour {
 		player=GameObject.FindGameObjectWithTag("Player").transform;
 		targets.Add(player.transform);
 		enemyState = gameObject.GetComponent<EnemyState>();
+		anim = gameObject.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
 		stunnedTime += Time.deltaTime;
-		timeSinceLastShot+=Time.deltaTime;
+
 		Vector3 newPosition = transform.position;
 		if(stunnedTime>stunDelay)
 		{
+			anim.SetBool("IsStunned",false);
+			timeSinceLastShot+=Time.deltaTime;
+			spawnpos=transform.position;
 			if(facingRight)
 			{
 				newPosition.x += moveSpeed * Time.deltaTime; 
-				Vector3 spawnpos=transform.position;
 				spawnpos.x+=(collider2D.bounds.size.x/1.5f);
-				if(timeSinceLastShot>=shotDelay)
-				{
-					if((player.position.x>=transform.position.x))
-					{
-						timeSinceLastShot=0;
-						GameObject bulletShot=GameObject.Instantiate(BulletPrefab,spawnpos,transform.rotation)as GameObject;
-						Vector2 tempVector= new Vector2((player.position.x - transform.position.x),player.position.y - transform.position.y);
-						tempVector.Normalize();
-						tempVector=tempVector*8;
-						bulletShot.rigidbody2D.velocity= tempVector; 
-					}
-				}
 			}
 			else
 			{
 				newPosition.x -= moveSpeed * Time.deltaTime; 
-				Vector3 spawnpos=transform.position;
 				spawnpos.x-=(collider2D.bounds.size.x/1.5f);
-				if(timeSinceLastShot>=shotDelay)
+				
+			}
+			Debug.Log(spawnpos);
+			if(timeSinceLastShot>shotDelay && (((player.position.x<=transform.position.x)&&!facingRight)||((player.position.x>=transform.position.x)&&facingRight)))
+			{
+
+
+				timeSinceLastShot=0;
+				isAttackingTimer=0;
+				anim.SetFloat("TimeAttacking",isAttackingTimer);
+
+			}
+			else if(isAttackingTimer<0.5f)
+			{
+				isAttackingTimer+=Time.deltaTime;
+				if(isAttackingTimer>0.25f)
 				{
-					if((player.position.x<=transform.position.x))
+					anim.SetFloat("TimeAttacking",isAttackingTimer);
+					GameObject bulletShot=GameObject.Instantiate(BulletPrefab,spawnpos,transform.rotation)as GameObject;
+					Vector2 tempVector= new Vector2((player.position.x - transform.position.x),player.position.y - transform.position.y);
+					tempVector.Normalize();
+					tempVector=tempVector*8;
+					bulletShot.rigidbody2D.velocity= tempVector; 
+					isAttackingTimer=1;
+				}
+
+			}
+			else
+			{
+				transform.position=newPosition;
+			}
+				/*
+					else if((!(timeSinceLastShot<shotDelay*0.9f||timeSinceLastShot<shotDelay*1.1f)))
 					{
-						timeSinceLastShot=0;
-						GameObject bulletShot=GameObject.Instantiate(BulletPrefab,spawnpos,transform.rotation)as GameObject;
-						Vector2 tempVector= new Vector2((player.position.x - transform.position.x),player.position.y - transform.position.y);
-						tempVector.Normalize();
-						tempVector=tempVector*8;
-						bulletShot.rigidbody2D.velocity= tempVector; 
+						anim.SetBool("IsAttacking",true);
+						
 					}
 				}
-			}
-			transform.position = newPosition;
+			}*/
+
 		}
 
 	}
@@ -81,6 +101,7 @@ public class AIController : MonoBehaviour {
 			KnockBack();
 			enemyState.TakeDamage(1);
 			stunnedTime=0;
+			anim.SetBool("IsStunned",true);
 		}
 	}
 
