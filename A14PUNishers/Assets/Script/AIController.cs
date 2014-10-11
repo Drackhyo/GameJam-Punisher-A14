475cd7a673620f3c261f6b9a081205b2a08414e4
Drackhyo <drackhyo@gmail.com>
2014-10-10 22:56:29 -0400

    1st bugfix-fix

--- A14PUNishers/Assets/Script/AIController.cs
+++ A14PUNishers/Assets/Script/AIController.cs
@@ -22,16 +22,17 @@ public class AIController : MonoBehaviour {
 		timeSinceLastShot+=Time.deltaTime;
 		if(timeSinceLastShot>=shotDelay)
 		{
+			GameObject bulletShot;
 			timeSinceLastShot=0;
 			if(player.position.x<transform.position.x)
 			{
 				Vector3 spawnpos=transform.position;
-				spawnpos.x-=2;
-				GameObject bulletShot=GameObject.Instantiate(BulletPrefab,spawnpos,transform.rotation)as GameObject;
+				spawnpos.x-=0.75f;
+				bulletShot=GameObject.Instantiate(BulletPrefab,spawnpos,transform.rotation)as GameObject;
 			}
 			else
 			{
-				GameObject bulletShot=GameObject.Instantiate(BulletPrefab,transform.position,transform.rotation)as GameObject;
+				bulletShot=GameObject.Instantiate(BulletPrefab,transform.position,transform.rotation)as GameObject;
 			}
 			bulletShot.rigidbody2D.velocity= new Vector2((player.position.x - transform.position.x),player.position.y - transform.position.y);
 			
