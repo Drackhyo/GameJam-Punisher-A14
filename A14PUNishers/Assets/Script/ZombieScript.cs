68e1c0e2d1d3d633f7f07f7e1c0ea811beffc20d
Drackhyo <drackhyo@gmail.com>
2014-10-11 21:47:21 -0400

    Zombie GET

---
+++ A14PUNishers/Assets/Script/ZombieScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieScript : MonoBehaviour {
+	private bool facingRight=true;
+	public float moveSpeed=1;
+	private Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 newPosition=transform.position;
+		if(facingRight)
+		{
+			newPosition.x += moveSpeed * Time.deltaTime; 
+		}
+		else
+		{
+			newPosition.x -= moveSpeed * Time.deltaTime; 		
+		}
+		transform.position=newPosition;
+	}
+	public void Flip ()
+	{
+			facingRight = !facingRight;
+			
+			Vector3 theScale = transform.localScale;
+			theScale.x *= -1;
+			transform.localScale = theScale;
+	}
+}
