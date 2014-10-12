6c83ba72d35119d20adf2b38255d4caba4fc65f1
Drackhyo <drackhyo@gmail.com>
2014-10-12 15:40:39 -0400

    `;sd

---
+++ A14PUNishers/Assets/BodyIsReady.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BodyIsReady : MonoBehaviour {
+	float fadeOut=0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up);
+		if (hit.collider != null)
+		{
+			if(hit.collider.gameObject.tag=="Player")
+			{
+				fadeOut=0.5f;
+				transform.FindChild("Hover").GetComponent<SpriteRenderer>().enabled=true;
+			}
+		}
+		fadeOut-=Time.deltaTime;
+		if(fadeOut<=0)
+			transform.FindChild("Hover").GetComponent<SpriteRenderer>().enabled=false;
+
+	}
+
+}
