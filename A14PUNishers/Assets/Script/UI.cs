8d08bde8c7f71c13032c5a966f26d1324bcb3fc2
TlclT <jordan.chayer@gmail.com>
2014-10-12 14:02:39 -0400

    fffffffffffffffffffuuuuuuuuuuuuuuuuuuuuu

---
+++ A14PUNishers/Assets/Script/UI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI : MonoBehaviour {
+
+	public Texture bgTexture;
+	public Texture lifeBar;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI()
+	{
+		GUI.DrawTexture(new Rect(10, 10, 400, 160), bgTexture);
+		GUI.DrawTexture(new Rect(95, 20, 295, 50), lifeBar);
+	}
+}
