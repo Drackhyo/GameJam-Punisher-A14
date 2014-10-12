c2e2bc7521a45560c259cc6856827d7f54a41c7e
Gab <ghebert10@gmail.com>
2014-10-11 20:03:01 -0400

    body ready (not)

--- A14PUNishers/Assets/Script/EnemyState.cs
+++ A14PUNishers/Assets/Script/EnemyState.cs
@@ -5,6 +5,11 @@ public class EnemyState : MonoBehaviour {
 
 	int health;
 
+	float deathDelay = 0.4f;
+	bool dead = false;
+
+	public GameObject body;
+
 	void Start()
 	{
 		if (rigidbody2D.gameObject.tag == "Human")
@@ -17,6 +22,10 @@ public class EnemyState : MonoBehaviour {
 	{
 		if (health <= 0)
 			Dies();
+
+		if(dead){
+			deathDelay -= Time.deltaTime;
+		}
 	}
 
 	public void TakeDamage(int amount)
@@ -29,12 +38,17 @@ public class EnemyState : MonoBehaviour {
 	{
 		if (rigidbody2D.gameObject.tag == "Human")
 		{
-			//human
+			GetComponent<Animator>().SetBool("Dead", true);
+
+			if(deathDelay <= 0){
+
+				Destroy(gameObject);
+			}
 		}
 		else
 		{
-
+			Destroy(gameObject);
 		}
-		Destroy(gameObject);
+
 	}
 }
