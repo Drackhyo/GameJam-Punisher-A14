b0c5199afa2da8d20c51b90ff333d4f2e02498fb
TlclT <jordan.chayer@gmail.com>
2014-10-11 23:53:57 -0400

    blabla

---
+++ A14PUNishers/Assets/Script/AnimIntro.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimIntro : MonoBehaviour {
+	/*
+	int state;
+	GameObject bacorn;
+	GameObject rainbow;
+
+	// Use this for initialization
+	void Start () {
+		state = 0;
+		bacorn = GameObject.FindGameObjectWithTag("bacorn");
+		rainbow = GameObject.FindGameObjectWithTag("rainbow");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		while(state < 4)
+		{
+			if(state == 0)
+			{
+				bacorn.rigidbody2D.transform.; //bouger la bacorn
+
+				if(bacorn.rigidbody2D.position() == Vector2())
+				{
+					state++;
+				}
+			}
+
+			if(state == 1)
+			{
+				//on met du texte
+				if(Input.GetKeyUp(KeyCode.Space))
+				{
+					state++;
+				}
+			}
+
+			if(state == 2)
+			{
+				rainbow.rigidbody2D.transform. //un scale
+				if(rainbow a la bonne hauteur)
+				{
+					state++;
+				}
+			}
+
+			if(state == 3)
+			{
+				//Anim perso se lève
+			}
+		}
+	}
+*/
+}
